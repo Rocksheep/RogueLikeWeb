d415e0af8b4591cf5f5543a1372a8b64722f4e1e
Rien <rien@dailydialogues.nl>
2017-07-17 19:09:28 +0200

    Upgraded the tile engine to show different tile images

--- src/map/GameMap.js
+++ src/map/GameMap.js
@@ -1,7 +1,8 @@
-var GameMap = function(width, height, player) {
+var GameMap = function(width, height, player, entitySprite) {
     this.width = width;
     this.height = height;
     this.player = player;
+    this.entitySprite = entitySprite;
     this.tiles = this.initializeTiles();
 };
 
@@ -63,6 +64,71 @@ GameMap.prototype.makeMap = function(maxRooms, minRoomSize, maxRoomSize) {
             numberOfRooms++;
         }
     }
+
+
+    // determine tile types
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+
+            //inner corner pieces
+            var succeeded = false;
+            if ((x > 0 && x < this.width - 1 && y > 0 && y < this.height - 1)) {
+                if (!this.tiles[x+1][y].blockSight && !this.tiles[x][y-1].blockSight &&
+                    this.tiles[x-1][y].blockSight && this.tiles[x][y+1].blockSight) {
+                    this.tiles[x][y].type = 4;
+                    succeeded = true;
+                }
+                else if (!this.tiles[x-1][y].blockSight && !this.tiles[x][y+1].blockSight &&
+                    this.tiles[x+1][y].blockSight && this.tiles[x][y-1].blockSight) {
+                    this.tiles[x][y].type = 6;
+                    succeeded = true;
+                }
+                else if (!this.tiles[x+1][y].blockSight && !this.tiles[x][y+1].blockSight &&
+                    this.tiles[x-1][y].blockSight && this.tiles[x][y-1].blockSight) {
+                    this.tiles[x][y].type = 5;
+                    succeeded = true;
+                }
+                else if (!this.tiles[x-1][y].blockSight && !this.tiles[x][y-1].blockSight &&
+                    this.tiles[x+1][y].blockSight && this.tiles[x][y+1].blockSight) {
+                    this.tiles[x][y].type = 3;
+                    succeeded = true;
+                }
+            }
+            if (!succeeded) {
+                //vertical pieces
+                if ((x < this.width - 1 && !this.tiles[x+1][y].blockSight) ||
+                    (x > 0 && !this.tiles[x-1][y].blockSight)) {
+                    this.tiles[x][y].type = 1;
+                }
+
+                //horizontal pieces
+                else if((y < this.height - 1 && !this.tiles[x][y+1].blockSight) ||
+                    (y > 0 && !this.tiles[x][y-1].blockSight)) {
+                    this.tiles[x][y].type = 2;
+                }
+
+                //upper left corner
+                else if(x < this.width - 1 && y < this.height - 1 && !this.tiles[x+1][y+1].blockSight) {
+                    this.tiles[x][y].type = 3;
+                }
+                //upper right corner
+                else if(x > 0 && y < this.height - 1 && !this.tiles[x-1][y+1].blockSight) {
+                    this.tiles[x][y].type = 4;
+                }
+                //bottom right corner
+                else if(x > 0 && y > 0 && !this.tiles[x-1][y-1].blockSight) {
+                    this.tiles[x][y].type = 5;
+                }
+                //bottom left corner
+                else if(x < this.width - 1 && y > 0 && !this.tiles[x+1][y-1].blockSight) {
+                    this.tiles[x][y].type = 6;
+                }
+                else {
+                    this.tiles[x][y].type = 0;
+                }
+            }
+        }
+    }
 };
 
 GameMap.prototype.createRoom = function(room) {
@@ -98,7 +164,33 @@ GameMap.prototype.draw = function(ctx, colors) {
         for (var x = 0; x < this.width; x++) {
             var wall = this.tiles[x][y].blockSight;
             var visible = fovMap[x][y];
-            if (visible) {
+            if (this.tiles[x][y].explored) {
+                if (wall) {
+                    if (this.tiles[x][y].type === 1) {
+                        ctx.drawImage(this.entitySprite, 16, 0, 16, 16, x*16, y*16, 16, 16);
+                    }
+                    if (this.tiles[x][y].type === 2) {
+                        ctx.drawImage(this.entitySprite, 32, 0, 16, 16, x*16, y*16, 16, 16);
+                    }
+                    if (this.tiles[x][y].type === 3) {
+                        ctx.drawImage(this.entitySprite, 16*4, 0, 16, 16, x*16, y*16, 16, 16);
+                    }
+                    if (this.tiles[x][y].type === 4) {
+                        ctx.drawImage(this.entitySprite, 16*5, 0, 16, 16, x*16, y*16, 16, 16);
+                    }
+                    if (this.tiles[x][y].type === 5) {
+                        ctx.drawImage(this.entitySprite, 16*6, 0, 16, 16, x*16, y*16, 16, 16);
+                    }
+                    if (this.tiles[x][y].type === 6) {
+                        ctx.drawImage(this.entitySprite, 16*3, 0, 16, 16, x*16, y*16, 16, 16);
+                    }
+                }
+                else {
+                    ctx.drawImage(this.entitySprite, 16 * 7, 0, 16, 16, x*16, y*16, 16, 16);
+                }
+            }
+            if (visible || (this.player.x === x && this.player.y === y)) {
+                ctx.globalAlpha = 0.4;
                 if (wall) {
                     ctx.fillStyle = colors.lightWall;
                 }
@@ -107,15 +199,7 @@ GameMap.prototype.draw = function(ctx, colors) {
                 }
                 this.tiles[x][y].explored = true;
                 ctx.fillRect(x*16, y*16, 16, 16);
-            }
-            else if (this.tiles[x][y].explored) {
-                if (wall) {
-                    ctx.fillStyle = colors.darkWall;
-                }
-                else {
-                    ctx.fillStyle = colors.darkGround;
-                }
-                ctx.fillRect(x*16, y*16, 16, 16);
+                ctx.globalAlpha = 1.0;
             }
         }
     }
